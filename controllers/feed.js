61a80e1a012d2ae91672ea1a6fa550cb695fc51f
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-10-05 17:54:40 +0300

    create codepen for testing api2

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,7 +4,29 @@ exports.getPosts = (req, res, next) => {
     res
         .status(200)
         .json({
-            posts: [{ title: 'First Post', content: 'This is the first post!' }]
+            posts: [
+                {
+                    title: 'First Post',
+                    content: 'This is the first post!'
+                },
+                   {
+                    title: 'second Post',
+                    content: 'This is the first post!'
+                },
+                      {
+                    title: 'Third Post',
+                    content: 'This is the first post!'
+                },
+                         {
+                    title: 'Fourth Post',
+                    content: 'This is the first post!'
+                },
+                            {
+                    title: 'Fifth Post',
+                    content: 'This is the first post!'
+                }
+                         
+            ]
         });
 };
 
